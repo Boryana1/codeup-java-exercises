2f9f8336d9fa249ee6201fe32a32ed028306f5fd
Boryana1 <boryana.dimova1@gmail.com>
2022-05-19 15:38:43 -0500

    added a loop

--- src/Bob.java
+++ src/Bob.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("Talk to Bob:");
-        String myStringInput = userInput.nextLine();
-        if (myStringInput.endsWith("?")) {
-            System.out.println("Sure");
-        }else if (myStringInput.equals("")) {
-            System.out.println("Fine. Be that way!");
-        } else if (myStringInput == myStringInput.toUpperCase()){
-            System.out.println("Whoa, chill out!");
-        } else{
-            System.out.println("Whatever.");
-        }
+            Scanner userInput = new Scanner(System.in);
+            String myStringInput;
+        do {
+            System.out.println("Talk to Bob:");
+            myStringInput = userInput.nextLine();
+            if (myStringInput.endsWith("?")) {
+                System.out.println("Sure");
+            } else if (myStringInput.equals("")) {
+                System.out.println("Fine. Be that way!");
+            } else if (myStringInput == myStringInput.toUpperCase()) {
+                System.out.println("Whoa, chill out!");
+            } else {
+                System.out.println("Whatever.");
+            }
+        }while(!myStringInput.equals("bye"));
     }
 }
