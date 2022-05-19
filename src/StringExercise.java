9a845a0404ca373a501a34bdff2d861fa1103cc9
Boryana1 <boryana.dimova1@gmail.com>
2022-05-19 15:29:42 -0500

    strings 1st exersice

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class StringExercise {
+public class StringExercise {
+    public static void main(String[] args) {
+        String number1 = "We don't need no education\nWe don't need no thought control";
+        System.out.println(number1);
+        String number2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(number2);
+        String number3 = "In windows, the main drive is usually C:\\";
+        System.out.println(number3);
+        String number4 = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(number4);
+    }
 }
