4424e1cb28f7a9593e20b0ca03070427443446d9
Boryana1 <boryana.dimova1@gmail.com>
2022-05-20 10:58:41 -0500

    done

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Person {
+import java.sql.SQLOutput;
+
+public class Person {
+    private String name;
+    public String getName(){
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name){
+//TODO: change the name field to the passed value
+        this.name = name;
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.printf("%s says hello!", this.name);
+    }
+//        Person p1 = new Person();
+    public Person(String personName){
+        name = personName;
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Anna");
+        p1.sayHello();
+    }
 }
