d522d102efb4354561022ba0ca3557dd3a2a76bd
Boryana1 <boryana.dimova1@gmail.com>
2022-05-27 19:32:51 -0500

    done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,42 +2,123 @@ import java.util.Scanner;
 
 
 public class HelloWorld {
+
         public static void main(String [] args) {
-////            1:
+              System.out.println("Hello, World!");
+
+//            Scanner newScanner = new Scanner(System.in);
+//            System.out.println("How old are you?");
+//            int newInput = newScanner.nextInt();
+//            int updatedInput = newScanner.nextInt();
+//            System.out.println(newInput);
+//            System.out.println(updatedInput);
+
+//TODO: 1.Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+
 //            System.out.println("Hello, World!");
 //            int myFavoriteNumber = 2;
 //            System.out.println(myFavoriteNumber);
-////            2:
+//
+////TODO: 2.Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+
 //            String myString = "myString";
 //            System.out.println(myString);
-////            3:
+//
+////TODO: 3.Change your code to assign a character value to myString.
 //            myString = "newString";
 //            System.out.println(myString);
-////            4:
+//
+////TODO: 4.Change your code to assign the value 3.14159 to myString.
+
 //            myString = String.valueOf(3.14159);
 //            System.out.println(myString);
-////            5:
+//
+////TODO: 5.Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console.
+
 //            long myNumber;
 ////            System.out.println(myNumber);
-////            6:
+//
+////TODO: 6.Change your code to assign the value 3.14 to myNumber
+// .
 //            myNumber = (long) 3.14;
-////            7:
+//            System.out.println(myNumber);
+//
+////TODO: 7.Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+
 //            myNumber = 123L;
 //            System.out.println(myNumber);
-////            8:
-//            myNumber = 123;
 //
-//            return myString;
-            Scanner newScanner = new Scanner(System.in);
-            System.out.println("How old are you?");
-            int newInput = newScanner.nextInt();
-            int updatedInput = newScanner.nextInt();
-            System.out.println(newInput);
-            System.out.println(updatedInput);
+////TODO: 8.Change your code to assign the value 123 to myNumber.
 
+//            myNumber = 123;
+//            System.out.println(myNumber);
+//
+////TODO: 9.Change your code to declare myNumber as a float. Assign the value 3.14 to it.
 
+//            float myFloatNumber =(float)myNumber;
+//            myFloatNumber = (float) 3.14;
+//            System.out.println(myFloatNumber);
 
+//TODO: 10. a)Copy and paste the following code blocks one at a time and execute them
+//int x = 5;
+//System.out.println(x++);
+//System.out.println(x);
 
+//int x = 5;
+//System.out.println(++x);
+//System.out.println(x);
+
+//TODO: 10. b)What is the difference between the above code blocks? Explain why the code outputs what it does.
+
+//            ++x increments the value of x and then returns x
+//            x++ returns the value of x and then increments
+
+//TODO: 11.Try to create a variable named class. What happens?
+
+//            String class = "String"; ---> You cannot use keywords like int, for, class, etc as variable name (or identifiers)
+
+//            as they are part of the Java programming language syntax. Here's the complete list of all keywords in Java programming.
+
+//TODO: 12.Object is the most generic type in Java. You can assign any value to a variable of type Object. What do you think will happen when the following code is run?
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+////      int three = (int) o;   //--> String cannot be cast to class java.lang.Integer
+//        System.out.println(o);
+//Copy and paste the code above and then run it. Does the result match with your expectation?
+//
+//How is the above example different from the code block below?
+//
+//
+//        int three = (int) "three";  //-->String cannot be converted to int
+//        System.out.println(three);
+//What are the two different types of errors we are observing?
+
+//TODO: 13.Rewrite the following expressions using the relevant shorthand assignment operators:
+//
+//int x = 4;
+//x = x + 5;
+//
+//        int x = 4;
+//        x += 5;
+//
+//int x = 3;
+//int y = 4;
+//y = y * x;
+//
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+//int x = 10;
+//int y = 2;
+//x = x / y;
+//y = y - x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
         }
 }
 
