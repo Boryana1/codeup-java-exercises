6f0abc550d944afed1d6613c9f4d47fb3cf03162
Boryana1 <boryana.dimova1@gmail.com>
2022-05-27 19:33:54 -0500

    update

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -4,26 +4,45 @@ import java.util.Scanner;
 //TODO: Inside of your shapes directory, create a class named Rectangle. It should have protected properties for both length
 // and width. Rectangle should define a constructor that accepts two numbers for length and width, and sets those properties.
 
-public class Rectangle {
-    private int length;
-    private int width;
-
-    public void Rectangle(int length, int width) {
-        this.length = length;
-        this.width = width;
-    }
-//TODO: // Create two methods on the Rectangle class, getArea and getPerimeter that return the respective values.
-// The formulas for both follow:
-// perimeter = 2 x length + 2 x width
-// area = length x width
-
-    public int getParameter() {
-        return 2 * length + 2 * width;
-    }
-    public int getArea(){
-        return length * width;
-    }
+//public class Rectangle extends Quadrilateral implements Measurable{
+//    @Override
+//    public double getPerimeter() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public double getArea() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public double setLength() {
+//        return 0;
+//    }
+//
+//    @Override
+//    public double setWidth() {
+//        return 0;
+//    }
+//    private double length;
+//    private double width;
+//
+//    public void Rectangle(double length, double width) {
+//        this.length = length;
+//        this.width = width;
+//    }
+////TODO: // Create two methods on the Rectangle class, getArea and getPerimeter that return the respective values.
+//// The formulas for both follow:
+//// perimeter = 2 x length + 2 x width
+//// area = length x width
+//
+//    public double getParameter() {
+//        return 2 * length + 2 * width;
+//    }
+//    public double getArea(){
+//        return length * width;
+//    }
 //    public double getCircumference() {
 //        return (2 * Math.PI * radius);
 //    }
-}
+//}
