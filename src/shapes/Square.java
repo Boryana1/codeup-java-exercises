9e09486cd2f96393164c492120ee94e6e2acb8b0
Boryana1 <boryana.dimova1@gmail.com>
2022-05-27 19:34:44 -0500

    update

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,12 +1,22 @@
-package shapes;
-
-//TODO: Create a class named Square that extends Rectangle. Square should define a constructor that accepts one argument,
-// side, and calls the parent's constructor to set both the length and width to the value of side.
-public class Square extends Rectangle{
-    private int side;
-    public void Square(int side){
-        this.side = side;
-        super.Rectangle(side, side);
-        }
-    }
-
+//package shapes;
+//
+////TODO: Create a class named Square that extends Rectangle. Square should define a constructor that accepts one argument,
+//// side, and calls the parent's constructor to set both the length and width to the value of side.
+//public class Square extends Rectangle{
+////    private double side;
+////    public void Square(double side){
+////        this.side = side;
+////        super.Rectangle(side, side);
+////        }
+////
+//////TODO: In the Square class, override the getArea and getPerimeter methods with the following definitions for a square
+////// perimeter = 4 x side
+////// area = side ^ 2
+////    public double getParameter(){
+////        return 4 * side;
+////    }
+////    public double getArea(){
+////        return (double) Math.pow(side, 2);
+////    }
+//}
+//
