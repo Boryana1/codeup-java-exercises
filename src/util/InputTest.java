3f1878b17b5e9cca8a03957b7e8472f5a40c9f1e
Boryana1 <boryana.dimova1@gmail.com>
2022-06-01 12:24:32 -0500

    updated

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -6,17 +6,18 @@ import java.util.Scanner;
 
 public class InputTest {
 //    private Scanner newInput = new Scanner(System.in);
-    public static void main(String[] args) {
-        int minInt = 1;
-        int maxInt = 10;
-        double min = 1.1;
-        double max = 10.1;
+    public static void main(String[] args) throws Exception{
+//        int minInt = 1;
+//        int maxInt = 10;
+//        double min = 1.1;
+//        double max = 10.1;
         Input in = new Input();
-        in.getString();
-        in.yesNo();
-        in.getInt(minInt, maxInt);
+//        in.getString();
+//        in.yesNo();
+////        in.getInt(minInt, maxInt);
         in.getInt();
-        in.getDouble(min, max);
+////        in.getDouble(min, max);
         in.getDouble();
+
     }
 }
