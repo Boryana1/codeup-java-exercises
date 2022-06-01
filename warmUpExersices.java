6d6b5351b4342d05f9abe17ccff85f506e6ed17f
Boryana1 <boryana.dimova1@gmail.com>
2022-06-01 12:24:50 -0500

    new

--- warmUpExersices.java
+++ warmUpExersices.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class warmUpExersices {
+public class warmUpExersices {
+
+//    Create a function that takes the age in years and returns the age in days.
+//    example input: 65
+//    expected output: 23725
+//            2. Create a function that takes two numbers and a mathematical operator + - / * and will perform a calculation with the given numbers.
+//    example inputs: 4, '/', 2
+//    expected output: 2 (edited)
 }
