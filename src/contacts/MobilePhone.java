7714a75045538bb7ddac90310b26084caa102025
Boryana1 <boryana.dimova1@gmail.com>
2022-06-05 12:39:33 -0500

    update

--- src/contacts/MobilePhone.java
+++ src/contacts/MobilePhone.java
@@ -1,6 +1,8 @@
 package contacts;
-
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Formatter;
+
 
 public class MobilePhone {
 
@@ -18,7 +20,7 @@ public class MobilePhone {
 
         // Before adding a contact we are checking that its exist or not
 
-        if (findContact(contact.getName()) >= 0) {
+        if(findContact(contact.getName()) >= 0) {
             System.out.println("Contact is already on file");
         }
 
@@ -26,23 +28,22 @@ public class MobilePhone {
         myContacts.add(contact);
         return true;
     }
-
-    public boolean updateContact(Contacts oldContact, Contacts newContact) {
-        int foundPosition = findContact(oldContact);
-        if (foundPosition < 0) {
-            System.out.println(oldContact.getName() + " , was not found");
-            return false;
-        }
-
-        this.myContacts.set(foundPosition, newContact);
-        System.out.println(oldContact.getName() + ", was replaced with " + newContact.getName());
-        return true;
-    }
+//    public boolean updateContact(Contacts oldContact , Contacts newContact) {
+//        int foundPosition = findContact(oldContact);
+//        if(foundPosition < 0) {
+//            System.out.println(oldContact.getName() + " , was not found");
+//            return false;
+//        }
+//
+//        this.myContacts.set(foundPosition, newContact);
+//        System.out.println(oldContact.getName() + ", was replaced with " + newContact.getName());
+//        return true;
+//    }
 
     // Remove Contact
     public boolean removeContact(Contacts contact) {
         int foundPosition = findContact(contact);
-        if (foundPosition < 0) {
+        if(foundPosition < 0) {
             System.out.println(contact.getName() + " , was not found");
             return false;
         }
@@ -50,7 +51,6 @@ public class MobilePhone {
         System.out.println(contact.getName() + " was deleted");
         return true;
     }
-
     // Find Contact Position
     private int findContact(Contacts contact) {
         return this.myContacts.indexOf(contact);
@@ -58,10 +58,10 @@ public class MobilePhone {
 
     // Find Contact Names
     private int findContact(String contactName) {
-        for (int i = 0; i < myContacts.size(); i++) {
+        for(int i =0; i < myContacts.size(); i++) {
             // Creating another temporary object to hold the name and compare
             Contacts contacts = this.myContacts.get(i);
-            if (contacts.getName().equals(contactName)) {
+            if(contacts.getName().equals(contactName)) {
                 return i;
             }
         }
@@ -70,15 +70,14 @@ public class MobilePhone {
 
     // query contact
     public String queryContact(Contacts contact) {
-        if (findContact(contact) >= 0) {
+        if(findContact(contact) >= 0) {
             return contact.getName();
         }
         return null;
     }
-
     public Contacts queryContact(String name) {
         int position = findContact(name);
-        if (position >= 0) {
+        if(position >=0) {
             return this.myContacts.get(position);
         }
         return null;
@@ -86,13 +85,35 @@ public class MobilePhone {
 
 
     public void printContacts() {
-        System.out.println("Contact list");
-        for (int i = 0; i < this.myContacts.size(); i++) {
-            System.out.println((i + 1) + " ." +
-                    this.myContacts.get(i).getName() +
-                    " -> " + this.myContacts.get(i).getPhoneNumber());
+//        Scanner newScanner = new Scanner(System.in);
+//        System.out.println("What number would you like to go up to? You can choose from 1 to 10.");
+//        int userInput = newScanner.nextInt();
+//        System.out.print("number\t | squared\t | cubed \n-----\t | -----\t | -----\n");
+//        for(
+//                int i = 1;
+//                i <=userInput;i++)
+//
+//        {
+//            int squared = (int) Math.pow(i, 2);
+//            int cubed = (int) Math.pow(i, 3);
+//            System.out.printf("%s \t \t | %s \t \t | %s \n", i, squared, cubed);
+//        }
+
+        Formatter fmt = new Formatter();
+
+//        System.out.println("   Name | Phone number");
+        System.out.println(fmt.format("%20s %20s\n", "Name", "| Phone number"));
+        System.out.println("----------------------------");
+        for(int i = 0; i < this.myContacts.size(); i++) {
+            String Num = String.valueOf((i+1));
+            String contactsNames = this.myContacts.get(i).getName();
+            String contactsPhoneNum = this.myContacts.get(i).getPhoneNumber();
+            System.out.println(fmt.format("%2s. %18s | %20s\n", Num, contactsNames, contactsPhoneNum));
+
+//            System.out.println((i+1) + ". " +
+//                    contactsNames +
+//                    " | " + contactsPhoneNum);
         }
     }
 
 }
-
