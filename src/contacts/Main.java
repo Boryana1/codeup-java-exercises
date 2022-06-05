311e7f9bece4a3207c34bc396425eb377cc55588
Boryana1 <boryana.dimova1@gmail.com>
2022-06-05 12:39:15 -0500

    update

--- src/contacts/Main.java
+++ src/contacts/Main.java
@@ -1,5 +1,8 @@
 package contacts;
 
+import contacts.Contacts;
+import contacts.MobilePhone;
+
 import java.util.Scanner;
 
 public class Main {
@@ -10,19 +13,14 @@ public class Main {
     public static void main(String[] args) {
 
         boolean quite = false;
-        startPhone();
+
         printAction();
         while (!quite) {
-            System.out.println("\n Enter action: (6 to show available actions)");
+            System.out.println("Enter an option (1, 2, 3, 4 or 5)");
             int action = scanner.nextInt();
             scanner.nextLine();
 
             switch (action) {
-                case 0:
-                    System.out.println("\n Shuting down .. ");
-                    quite = true;
-                    break;
-
                 case 1:
                     mobilePhone.printContacts();
                     break;
@@ -32,16 +30,15 @@ public class Main {
                     break;
 
                 case 3:
-                    updateContact();
+                    queryContact();
                     break;
+
                 case 4:
                     removeContact();
                     break;
                 case 5:
-                    queryContact();
-                    break;
-                case 6:
-                    printAction();
+                    System.out.println("Exit");
+                    quite = true;
                     break;
 
 
@@ -50,87 +47,73 @@ public class Main {
     }
 
     private static void addNewContact() {
-        System.out.println("Enter new Contact Name: ");
+        System.out.println("Enter new contact name: ");
         String name = scanner.nextLine();
-        System.out.println("Enter phone Number: ");
+        System.out.println("Enter contact's phone Number: ");
         String phone = scanner.nextLine();
         String number = phone.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3");
-
         Contacts newContact = Contacts.createContact(name, number);
-        if(mobilePhone.addNewContact(newContact)) {
-            System.out.println("New Contact Added: name = "+ name + " , phone: " + number );
-        }else {
-            System.out.println("Can't add, " + name +" already on file");
+        if (mobilePhone.addNewContact(newContact)) {
+            System.out.println("New Contact Added: " + name + " | " + number);
+        } else {
+            System.out.println("Can't add, " + name + " already on file");
         }
     }
-    private static void updateContact() {
-        System.out.println("Enter existing contact name : ");
-        String name = scanner.nextLine();
-        Contacts extContact = mobilePhone.queryContact(name);
-        if(extContact == null)
-        {
-            System.out.println("Cannot found contact");
-            return;
-        }
 
-        System.out.println("Enter new Contact Name: ");
-        String newName = scanner.nextLine();
-        System.out.println("Enter new Phone Number: ");
-        String newNumber = scanner.nextLine();
-        String newNum = newNumber.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3");
-
-        Contacts newContact = Contacts.createContact(newName, newNum);
-        if(mobilePhone.updateContact(extContact, newContact))
-        {
-            System.out.println("Successfully updated");
-        }else {
-            System.out.println("Error Updating Contact");
-        }
-    }
     private static void removeContact() {
         System.out.println("Enter existing contact name : ");
         String name = scanner.nextLine();
         Contacts extContact = mobilePhone.queryContact(name);
-        if(extContact == null)
-        {
-            System.out.println("Cannot found contact");
+        if (extContact == null) {
+            System.out.println("Cannot find contact");
             return;
         }
 
-        if(mobilePhone.removeContact(extContact)) {
-            System.out.println("Successfully deleted");
-        }else {
-            System.out.println("Error Deleting Contact");
+        if (mobilePhone.removeContact(extContact)) {
+            System.out.println("Contact successfully deleted");
+        } else {
+            System.out.println("There was an error deleting the contact");
         }
     }
+
     private static void queryContact() {
-        System.out.println("Enter existing contact name : ");
+        System.out.println("Enter an existing contact name : ");
         String name = scanner.nextLine();
         Contacts extContact = mobilePhone.queryContact(name);
-        if(extContact == null)
-        {
-            System.out.println("Cannot found contact");
+        if (extContact == null) {
+            System.out.println("Contact does not exist");
             return;
         }
 
         System.out.println("Name : " + extContact.getName() +
-                " Phone Number is : " + extContact.getPhoneNumber());
+                " Phone Number is: " + extContact.getPhoneNumber());
     }
 
 
-    private static void startPhone() {
-        System.out.println("Starting Phone . . .");
-    }
     private static void printAction() {
-        System.out.println("\nAvailable actions:\npress");
-        System.out.println("0 - to shutDown\n" +
-                "1 - to print contacts\n"+
-                "2 - to add a new contact\n"+
-                "3 - to update an existing contact\n"+
-                "4 - to remove and existing contact\n"+
-                "5 - query for existing contact\n"+
-                "6 - to print a list of available actions.");
+        System.out.println("\nSelect from the available options: ");
+        System.out.println(
+                "1. View Contacts\n" +
+                        "2. Add a new contact\n" +
+                        "3. Search a contact by name\n" +
+                        "4. Delete an existing contact.\n" +
+                        "5. Exit\n");
         System.out.println("Choose Your Action: ");
     }
 
+
+    //-------------------------TABLE--------------------------
+//    Scanner newScanner = new Scanner(System.in);
+//        System.out.println("What number would you like to go up to? You can choose from 1 to 10.");
+//    int userInput = newScanner.nextInt();
+//                System.out.print("number\t | squared\t | cubed \n-----\t | -----\t | -----\n");
+//                for(
+//    int i = 1;
+//    i <=userInput;i++)
+//
+//    {
+//        int squared = (int) Math.pow(i, 2);
+//        int cubed = (int) Math.pow(i, 3);
+//        System.out.printf("%s \t \t | %s \t \t | %s \n", i, squared, cubed);
+//    }
 }
