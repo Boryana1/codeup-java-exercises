2d5908d6d198a19c02851b277c12b15536058a68
Boryana1 <boryana.dimova1@gmail.com>
2022-06-05 12:41:53 -0500

    new method

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,2 +1,40 @@
-package movies;public class MoviesApplication {
+package movies;
+
+import util.Input;
+
+import java.util.Scanner;
+
+public class MoviesApplication {
+//What would you like to do?
+//
+//0 - exit
+//1 - view all movies
+//2 - view movies in the animated category
+//3 - view movies in the drama category
+//4 - view movies in the horror category
+//5 - view movies in the scifi category
+//
+//Enter your choice: 1
+//
+//Frankenstein -- horror
+//Goodfellas -- drama
+//Pulp Fiction -- drama
+//...
+    public static void main(String[] args) {
+        System.out.println("What would you like to do?");
+        System.out.println("0 - exit");
+        System.out.println("1 - view all movies");
+        System.out.println("2 - view movies in the animated category");
+        System.out.println("3 - view movies in the drama category");
+        System.out.println("4 - view movies in the horror category");
+        System.out.println("5 - view movies in the scifi category");
+        System.out.println("Enter your choice:");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        if(input == 1){
+            System.out.println(MoviesArray.findAll());
+        }
+
+
+    }
 }
