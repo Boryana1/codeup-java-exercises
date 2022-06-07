feca345718541a0d0f0c08826c4bfc5275701e99
Boryana1 <boryana.dimova1@gmail.com>
2022-06-06 22:21:38 -0500

    cleaned up code

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,48 +1,38 @@
 package shapes;
-import java.util.Scanner;
 
 //TODO: Inside of your shapes directory, create a class named Rectangle. It should have protected properties for both length
 // and width. Rectangle should define a constructor that accepts two numbers for length and width, and sets those properties.
 
-//public class Rectangle extends Quadrilateral implements Measurable{
-//    @Override
-//    public double getPerimeter() {
-//        return 0;
-//    }
-//
-//    @Override
-//    public double getArea() {
-//        return 0;
-//    }
-//
-//    @Override
-//    public double setLength() {
-//        return 0;
-//    }
-//
-//    @Override
-//    public double setWidth() {
-//        return 0;
-//    }
-//    private double length;
-//    private double width;
-//
-//    public void Rectangle(double length, double width) {
-//        this.length = length;
-//        this.width = width;
-//    }
-////TODO: // Create two methods on the Rectangle class, getArea and getPerimeter that return the respective values.
-//// The formulas for both follow:
-//// perimeter = 2 x length + 2 x width
-//// area = length x width
-//
-//    public double getParameter() {
-//        return 2 * length + 2 * width;
-//    }
-//    public double getArea(){
-//        return length * width;
-//    }
-//    public double getCircumference() {
-//        return (2 * Math.PI * radius);
-//    }
-//}
+public class Rectangle extends Quadrilateral implements Measurable{
+    private double length;
+    private double width;
+    public Rectangle(double length, double width) {
+        super(length, width);
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double setLength(double length) {
+        return length;
+    }
+
+    @Override
+    public double setWidth(double width) {
+        return width;
+    }
+
+
+
+//TODO: // Create two methods on the Rectangle class, getArea and getPerimeter that return the respective values.
+// The formulas for both follow:
+// perimeter = 2 x length + 2 x width
+// area = length x width
+
+    public double getParameter() {
+        return 2 * length + 2 * width;
+    }
+    public double getArea(){
+        return length * width;
+    }
+}
