2d40bca0a5ff3affdaf0480d9fada8f33a885519
Boryana1 <boryana.dimova1@gmail.com>
2022-06-08 09:38:14 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # codeup-java-exercises
+# database-exercises
--- src/grades/StudentTest.java
+++ src/grades/StudentTest.java
@@ -1,5 +1,7 @@
 package grades;
 
+import java.util.Scanner;
+
 public class StudentTest {
 public static void main(String[] args) {
 
@@ -32,5 +34,10 @@ public static void main(String[] args) {
 //    create a HashMap named students. It should have keys that are strings that represent github usernames,
 //    and values that are Student objects. Create at least 4 Student objects with at least 3 grades each, and add them to the map.
 //            Be creative! Make up GitHub usernames and grades for your student objects.
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Welcome");
+    String tryAgain = "";
+
 }
 }
